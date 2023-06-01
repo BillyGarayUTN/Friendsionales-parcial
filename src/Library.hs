7c78d5c4afeb261ea09774495ca82e42ab3f64b0
BillyGarayUTN <bgarayquispe@frba.utn.edu.ar>
2023-06-01 01:46:32 -0300

    resuelto hasta el punto 2

--- src/Library.hs
+++ src/Library.hs
@@ -20,16 +20,39 @@ data Persona = Persona{
     tareas :: [Persona->Persona]
 } deriving (Show,Eq)
 
+
+--Punto 1
+
 nivelDeEnergia :: Persona -> Number
 nivelDeEnergia persona
     | nivelDeAlegria persona > nivelDeAnsiedad persona =(min 340 ).(*2).nivelDeAlegria $ persona 
+    | (esJoven persona) && (nivelDeAnsiedad persona > nivelDeAlegria persona) = (300-).nivelDeEstres $ persona
+    | otherwise = (10+).nivelDeAlegria $ persona 
+
+nivelDeEstres :: Persona -> Number
+nivelDeEstres persona
+    | (5<).length.tareas $ persona   = nivelDeAnsiedad persona * 1.5
+    | otherwise = nivelDeAnsiedad persona
+
+esJoven :: Persona -> Bool
+esJoven persona = (<40).edad $ persona
+
+-- Punto 2
+viejoSonLosTrapos :: [Persona] -> Bool
+viejoSonLosTrapos grupoPersonas = all (esVital) grupoPersonas
+
+esVital :: Persona -> Bool
+esVital persona =  ((not.esJoven) persona) && (((>100).nivelDeEnergia) persona )
+
+nivelTotalDeAnsiedad :: [Persona] -> Number
+nivelTotalDeAnsiedad grupoPersonas = sumOf nivelDeAnsiedad grupoPersonas
 
 
+-- ansiedadMayora50 persona = (50<).nivelDeAnsiedad persona 
 
 
-
-
-
+losMasCriticados :: (Persona->Bool) ->  
+losMasCriticados criterio 
 
 
 
